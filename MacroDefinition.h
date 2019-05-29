943fe5f2c42a03f3d62dec10c7d3e34682b30ee5
常露阳 <changluyang@hualala.com>
2019-05-29 10:51:57 +0800

    单例化一个类的宏

--- MacroDefinition.h
+++ MacroDefinition.h
@@ -206,44 +206,46 @@ userInfo:aUserInfo]
 #define degreesToRadian(x) (M_PI * (x) / 180.0)
 #define radianToDegrees(radian) (radian*180.0)/(M_PI)
 
+// 单例化一个类
 
+/**
+ *  在.h文件中定义的宏，arc
+ *
+ *  SISingletonH(name) 这个是宏
+ *  + (instancetype)shared##name;这个是被代替的方法， ##代表着shared+name 高度定制化
+ * 在外边我们使用 “SISingletonH(gege)” 那么在.h文件中，定义了一个方法"+ (instancetype)sharedgege",所以，第一个字母要大写
+ *
+ *  @return 一个搞定好的方法名
+ */
+#define SUPSingletonH(name) + (instancetype)shared##name;
 
-//单例化一个类
-#define SYNTHESIZE_SINGLETON_FOR_CLASS(classname) \
-\
-static classname *shared##classname = nil; \
-\
-+ (classname *)shared##classname \
-{ \
-@synchronized(self) \
-{ \
-if (shared##classname == nil) \
-{ \
-shared##classname = [[self alloc] init]; \
-} \
-} \
-\
-return shared##classname; \
-} \
-\
-+ (id)allocWithZone:(NSZone *)zone \
-{ \
-@synchronized(self) \
-{ \
-if (shared##classname == nil) \
-{ \
-shared##classname = [super allocWithZone:zone]; \
-return shared##classname; \
-} \
-} \
-\
-return nil; \
-} \
-\
-- (id)copyWithZone:(NSZone *)zone \
-{ \
-return self; \
+/**
+ *  在.m文件中处理好的宏 arc
+ *
+ *  SISingletonM(name) 这个是宏,因为是多行的东西，所以每行后面都有一个"\",最后一行除外，
+ * 之所以还要传递一个“name”,是因为有个方法要命名"+ (instancetype)shared##name"
+ *  @return 单利
+ */
+#define SUPSingletonM(name) \
+static name *instance_ = nil;\
++ (instancetype)shared##name{\
+static dispatch_once_t onceToken;\
+dispatch_once(&onceToken, ^{\
+instance_ = [[self alloc] init];\
+});\
+return instance_;\
+}\
++ (instancetype)allocWithZone:(struct _NSZone *)zone{\
+static dispatch_once_t onceToken;\
+dispatch_once(&onceToken, ^{\
+instance_ = [super allocWithZone:zone];\
+});\
+return instance_;\
+}\
+- (id)copyWithZone:(NSZone *)zone{\
+return instance_;\
 }
+
 
 
 
